aeae0e72b2966fa35da56d7fb291e363f3333a68
Laboratorio de Software <lds@unac.edu.co>
2019-09-04 18:38:11 -0500

    Agenda

---
+++ Agenda.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Agenda.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Agenda/Agenda/views/DetailContacsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Agenda.Domain;
+using Agenda.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agenda.views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetailContacsPage : ContentPage
+	{
+		public DetailContacsPage (Contact contact)
+		{
+			InitializeComponent ();
+            LoadData(contact);
+		}
+
+        private void LoadData(Contact contact)
+        {
+            gridDatos.BindingContext = contact;
+
+        }
+	}
+}
